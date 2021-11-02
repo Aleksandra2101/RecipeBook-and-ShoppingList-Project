1e989e48cd66d156347316c1e6479d6feb4edc31
Aleksandra2101 <alexandra.stojanovska@yahoo.com>
2021-11-02 17:11:35 +0100

    new changes..

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,28 @@
+import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 
 export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+
+
    private  recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test',
-          'https://www.simplyrecipes.com/thmb/guUt8eMy7mZn6Aw62nVMWSorQ3U=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2018__07__Seafood-Paella-LEAD-VERTICAL-fc5f1e71baa8484cafa1a106ffaa9c23.jpg'),
+        new Recipe('A Test Recipe', 
+          'This is simply a test',
+          'https://www.simplyrecipes.com/thmb/guUt8eMy7mZn6Aw62nVMWSorQ3U=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2018__07__Seafood-Paella-LEAD-VERTICAL-fc5f1e71baa8484cafa1a106ffaa9c23.jpg',
+          [ 
+              new Ingredient('Meat',1),
+              new Ingredient('French fries',20)
+        ]),
+          
           new Recipe('Another Test Recipe', 'This is simply a test',
-          'https://www.simplyrecipes.com/thmb/guUt8eMy7mZn6Aw62nVMWSorQ3U=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2018__07__Seafood-Paella-LEAD-VERTICAL-fc5f1e71baa8484cafa1a106ffaa9c23.jpg')
+          'https://www.simplyrecipes.com/thmb/guUt8eMy7mZn6Aw62nVMWSorQ3U=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2018__07__Seafood-Paella-LEAD-VERTICAL-fc5f1e71baa8484cafa1a106ffaa9c23.jpg',
+          [
+            new Ingredient('Buns',2), 
+            new Ingredient('Meat',1)
+          ])
+
       ];
 
       getRecipes() {
