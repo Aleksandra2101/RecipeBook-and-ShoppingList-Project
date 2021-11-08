f67764998bfc03e36c2d8d2130a4e49bc0b10db6
Aleksandra Stojanovska <alexandra.stojanovska2101@gmail.com>
2021-11-08 17:25:04 +0100

    forms

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -33,8 +33,6 @@ export class RecipeDetailComponent implements OnInit {
   }
   onEditRecipe() {
    this.router.navigate(['edit'],{relativeTo:this.route});
-  
-
 
   }
 
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,6 @@
-import { EventEmitter, Injectable } from "@angular/core";
+import {  Injectable } from "@angular/core";
+import { Subject } from "rxjs";
+
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
@@ -6,7 +8,7 @@ import { Recipe } from "./recipe.model";
 
 @Injectable()
 export class RecipeService {
-    recipeSelected = new EventEmitter<Recipe>();
+  
 
 
    private  recipes: Recipe[] = [
