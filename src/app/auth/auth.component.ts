9886d18acbce67555ea3a5ca85dea67c5c72ddbc
Aleksandra Stojanovska <alexandra.stojanovska2101@gmail.com>
2021-12-13 17:06:26 +0100

    new changes

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,57 +1,48 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { AuthService,AuthResponseData} from './auth.service';
+import { AuthService, AuthResponseData } from './auth.service';
 
 @Component({
-  selector: 'app-auth',
-  templateUrl: './auth.component.html',
+    selector: 'app-auth',
+    templateUrl: './auth.component.html',
 })
 export class AuthComponent {
-  isLoginMode = true;
-  isLoading = false;
-  error:string = null;
-  constructor(private authService: AuthService) {}
+    isLoginMode = true;
+    isLoading = false;
+    error: string = null;
+    constructor(private authService: AuthService) { }
 
-  onSwitchMode() {
-    this.isLoginMode = !this.isLoginMode;
-  }
-
-  onSubmit(form: NgForm) {
-    if (!form.valid) {
-      return;
+    onSwitchMode() {
+        this.isLoginMode = !this.isLoginMode;
     }
-    const email = form.value.email;
-    const password = form.value.password;
-    let authObs:Observable<AuthResponseData>
-    
-    this.isLoading =true;
-    if (this.isLoginMode) {
-        this.authService.login(email,password).subscribe(
+
+    onSubmit(form: NgForm) {
+        if (!form.valid) {
+            return;
+        }
+        const email = form.value.email;
+        const password = form.value.password;
+        let authObs: Observable<AuthResponseData>
+
+        this.isLoading = true;
+        if (this.isLoginMode) {
+            authObs = this.authService.login(email, password);
+        } else {
+            authObs = this.authService.signup(email, password);
+        }
+        authObs.subscribe(
             (resData) => {
                 console.log(resData);
-               this.isLoading =false;
-      
-              },
-              (error) => {
+                this.isLoading = false;
+
+            },
+            (error) => {
                 this.error = 'An error occured!'
-                this.isLoading =false;
-              }
+                this.isLoading = false;
+            }
         );
-    } else {
-      this.authService.signup(email, password).subscribe(
-        (resData) => {
-          console.log(resData);
-         this.isLoading =false;
 
-        },
-        (error) => {
-          this.error = 'An error occured!'
-          this.isLoading =false;
-        }
-      );
+        form.reset();
     }
-
-    form.reset();
-  }
 }
